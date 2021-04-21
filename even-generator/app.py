993c0ad5268aa8f0ab36633d859fc8c9cdf811e8
Victor dos Reis Avelino da Silva <victor.silva@redt.com.br>
2021-04-21 19:35:10 -0300

    Even service fixes and created compose to run even and odd services.

--- even-generator/app.py
+++ even-generator/app.py
@@ -2,19 +2,24 @@ from flask import Flask, Response
 from flask_cors import CORS
 from datetime import datetime
 from even import even
+import json
+
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/even_service/number')
+@app.route("/even_service/number")
 def even_number():
     result = even()
     return Response(
         status=200, 
-        response={"number": result, "timestamp": datetime.now().timestamp()}
+        response=json.dumps({
+            "number": result, "timestamp": datetime.now().timestamp()
+        }),
+        mimetype="application/json"
     )
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
